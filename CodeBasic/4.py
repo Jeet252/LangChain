9f6f506bb3b98c8569a18ea19ea05c044e683267
Jeet252 <jeetnakrani2002@gmail.com>
2026-05-22 21:45:33 +0530

    4.py

---
+++ CodeBasic/4.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from google import genai
+from langchain_core.runnables import RunnablePassthrough
